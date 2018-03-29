ec64af3f6c198ea1c208f985c63239b87a0cf81c
Naieem Mahmud Supto <naieemsupto@gmail.com>
2018-03-29 18:39:05 +0600

    file upload complete

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var path = require('path');
+var upload = multer({ dest: 'uploads/' });
 
-/* GET home page. */
+/* GET main route. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.send(200, "storage service working");
 });
+
+router.post('/upload', upload.single('files'), function(req, res) {
+    console.log(path);
+    res.send(200, {
+        fileInfo: denormalizeFileInfo(req)
+    });
+});
+/**
+ * Denormalizing files information to return in the response 
+ * @params {params} 
+ * @returns {returns}
+ */
+function denormalizeFileInfo(req) {
+    return {
+        filename: req.file.filename,
+        fileType: req.file.mimetype,
+        fileUrl: path.resolve("") + "/" + req.file.path,
+        fileSize: req.file.size
+    }
+}
 
 module.exports = router;
